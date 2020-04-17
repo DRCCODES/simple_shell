1597c117f17efd8c5334cc1ad6fdc0cfd438137f
DRCCODES <1723@holbertonschool.com>
2020-04-17 03:48:15 +0000

    compies takes args ctrl-c is a heart

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include "shell.h"
+
 /**
 * sigintHandler - ignores ctrl c to prevent acc closure
+* 
+* @sig_unm: Dummy argument
 *
 * Return: N/A
+*
 */
-void sigintHandler()
+void sigintHandler(int sig_num)
 {
+	int i = sig_num;
+	i = 0;
+	i++;
+	i--;
+
 	signal(SIGINT, sigintHandler);
 	printf("\n$ ");
 	fflush(stdout);
 }
-/**
-* main - main
-* @argc: count of arguments
-* @args: arguments
-* @envp: env variables
-*
-* Return: 0
-*/
+
+
 int main(int argc, char **args, char **envp)
 {
 char *path = (char *)malloc(sizeof(char) * 256);
@@ -27,7 +30,9 @@ if (strcmp("exit", args[0]) == 0)
 exit(98);
 copy_envp(envp, my_envp);
 get_path(my_envp, path);
+
 signal(SIGINT, sigintHandler);
+
 for (;;)
 {
 char *array[sizeof(path) + 1];
@@ -42,16 +47,16 @@ split_path(x, array);
 _fputs("$ ");
 c = getline(&ptr, &size, stdin);
 if (c == EOF)
-	return (0);
+	exit(98);
 if (*ptr == '\n' || argc == 0)
 	continue;
 split_to_args(ptr, args);
 if (strcmp("exit", args[0]) == 0)
-	return (0);
+	exit(98);
 args[0] = path_check(args[0], array, path);
-check = is_it_there(path_check(args[0], array, path));
 if (fork() == 0)
 exit(execve(args[0], args, my_envp));
+check = is_it_there(args[0]);
 if (check != 1)
 {
 _fputs(args[0]);
@@ -61,3 +66,4 @@ free_args(args);
 wait(&wstatus);
 }
 }
+
